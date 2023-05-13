5a6833445250487060645d06ed796e00c6979204
9deok <butterflynine20@gmail.com>
2023-05-13 16:28:45 +0900

    OrderEntiy datamapper, repository, adapter 셋팅

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderItemEntity.java
@@ -0,0 +1,64 @@
+package com.food.ordering.system.order.service.dataaccess.order.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(OrderItemEntityId.class)
+@Table(name = "order_items")
+@Entity
+public class OrderItemEntity {
+
+    @Id
+    private Long id;
+
+    @Id
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ORDER_ID")
+    private OrderEntity order;
+
+    private UUID productId;
+    private BigDecimal price;
+    private Integer quantity;
+    private BigDecimal subTotal;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        OrderItemEntity that = (OrderItemEntity) o;
+
+        if (!id.equals(that.id)) {
+            return false;
+        }
+        return order.equals(that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + order.hashCode();
+        return result;
+    }
+}
